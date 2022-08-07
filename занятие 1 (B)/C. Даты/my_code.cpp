2fcbd0815d2a79037835e755c3746b93560f7ede
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2022-08-07 18:21:25 +0300

    Create my_code.cpp

---
+++ занятие 1 (B)/C. Даты/my_code.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+// solution
+bool check_first_second(const int first, const int second) {
+	return ((first <= 12) && (12 < second || first == second));
+}
+
+int main() {
+
+	// input
+	int first, second;
+	cin >> first >> second; // можно ли не до конца считать данные, так как анализировать год не смысла ввиду ограничений на него
+
+	// output
+	if (check_first_second(first, second) || check_first_second(second, first)) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+
+	return 0;
+}
