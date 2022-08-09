e37ef46de5be04ada73dc1dfa0b89be53fb9999f
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2022-08-09 12:29:56 +0300

    Create my_code_with_asymptotics_n^2.cpp

---
+++ занятие 2 (B)/(bad_asymptotics) B. Дома и магазины/my_code_with_asymptotics_n^2.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+
+	const size_t count_building = 10;
+
+	vector<int> shops; // 2
+	vector<int> residential_buildings; // 1
+
+	for (int i = 0; i < count_building; ++i) {
+		int building;
+		cin >> building;
+
+		if (building == 2) {
+			shops.push_back(i);
+		}
+
+		if (building == 1) {
+			residential_buildings.push_back(i);
+		}
+
+	}
+
+	int ans = -1;;
+	for (const auto res_build : residential_buildings) {
+		int max_dist_from_min = 11;
+		for (const auto shop : shops) {
+
+			if (abs(shop - res_build) < max_dist_from_min) {
+				max_dist_from_min = abs(shop - res_build);
+			}
+
+		}
+
+		if (ans < max_dist_from_min) {
+			ans = max_dist_from_min;
+		}
+	}
+
+	cout << ans;
+	return 0;
+}
