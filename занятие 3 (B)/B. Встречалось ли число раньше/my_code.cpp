162fe4557f298ffe70182b93b5028202e3c091e9
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2022-08-12 14:23:27 +0300

    Update my_code.cpp

--- занятие 3 (B)/B. Встречалось ли число раньше/my_code.cpp
+++ занятие 3 (B)/B. Встречалось ли число раньше/my_code.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<unordered_set>
 
+using namespace std;
+
+int main() {
+
+	unordered_set <int> set_numbers;
+	int number;
+
+	while (cin >> number) {
+
+		if (set_numbers.count(number)) {
+			cout << "YES" << '\n';
+		}
+		else {
+			cout << "NO" << '\n';
+			set_numbers.insert(number);
+		}
+
+	}
+
+	return 0;
+}
