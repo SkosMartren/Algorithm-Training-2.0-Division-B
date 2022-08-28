f29862342531c63c456b000e9fc810ab29400820
SkosMartren <55439084+SkosMartren@users.noreply.github.com>
2022-08-28 22:26:11 +0300

    Update my_code.cpp

--- занятие 7 (B)/A. Закраска прямой/my_code.cpp
+++ занятие 7 (B)/A. Закраска прямой/my_code.cpp
@@ -1,7 +1,7 @@
 #include <algorithm> // sort
 #include <iostream>
 #include <vector>
-#include  <limits.h> // INT_MIN
+#include  <limits.h>
 
 using namespace std;
 
@@ -10,7 +10,7 @@ struct event {
     int x, type;
 };
 
-int scanline(vector<pair<int, int>> segments) {
+int scanline(const vector<pair<int, int>>& segments) {
     vector<event> events;
 
     for (const auto& [l, r] : segments) {
@@ -18,15 +18,16 @@ int scanline(vector<pair<int, int>> segments) {
         events.push_back({ r, -1 });
     }
 
-    sort(events.begin(), events.end(), [](event a, event b) {
+    sort(events.begin(), events.end(), [](const event& a, const event& b) {
         return (a.x < b.x || (a.x == b.x && a.type > b.type));
         });
 
     int cnt = 0, res = 0, prev = INT_MIN;
 
-    for (event e : events) {
-        if (prev != INT_MIN && cnt > 0)
+    for (const event& e : events) {
+        if (prev != INT_MIN && cnt > 0){
             res += e.x - prev; // весь отрезок [prev, e.x] покрыт cnt отрезками
+        }
         cnt += e.type;
         prev = e.x;
     }
